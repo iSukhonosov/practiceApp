cd3b1c299575a963a7c120fa3e7e11652b6c648a
iSukhonosov <117342960+iSukhonosov@users.noreply.github.com>
2022-11-03 10:36:45 +0300

    Add files via upload

---
+++ practice UIKit/practice UIKit/ViewControllerTwo.swift
@@ -0,0 +1,24 @@
+//
+//  ViewControllerTwo.swift
+//  practice UIKit
+//
+//  Created by Ilya Sukhonosov on 03.11.2022.
+//
+
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
---
+++ practice UIKit/practice UIKit/class UIView.swift
@@ -0,0 +1,12 @@
+//
+//  class UIView.swift
+//  practice UIKit
+//
+//  Created by Ilya Sukhonosov on 03.11.2022.
+//
+
+import UIKit
+
+class class_UIView: NSObject {
+
+}
